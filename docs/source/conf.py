a5eb33d3f0d2dc2657996f06f242933668388d2f
sandy1618 <sandeepnayak1.618@gmail.com>
2022-02-25 14:37:09 +0530

    Added additional sphinx options

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -53,3 +53,15 @@ html_theme = 'alabaster'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+# Add any Sphinx extension module names here, as strings. They can be
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# ones.
+extensions = [
+    'sphinx.ext.duration',
+]
+
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+#
+html_theme = 'furo'
